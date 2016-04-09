b61112b98dfa41600369ab4103512a3ac0f0c854
Val <valexx55@gmail.com>
2016-04-09 11:51:14 +0200

    Nuevo servicio ObtenerFechaUltimo para dar soporte a la nueva funcionalidad de Notificaciones y duplicación clases DTO para mantener compatibilidad en la serialización de clases DTO, debido a la refactorización en el cliente (nuevos paquetes para organizar las clases)

---
+++ RadioMarcaServer/src/com/marca/radio/boxeo/ObtenerFechaUltimo.java
@@ -0,0 +1,83 @@
+package com.marca.radio.boxeo;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ * Servlet implementation class ObtenerFechaUltimo
+ */
+@WebServlet("/ObtenerFechaUltimo")
+public class ObtenerFechaUltimo extends HttpServlet {
+	private static final long serialVersionUID = 918L;
+	
+	private final static Logger log = Logger.getLogger("mylog");
+
+	
+	private String obtenerFechaUltimo() 
+	{
+		String str_fecha_mayor = null;
+		String ruta = null;
+		String str_aux = null;
+		File dir = null;
+		File[] lista_programas = null;
+		File carpeta = null;
+		
+			ruta = getServletContext().getInitParameter("ruta_programas");//saco la ruta de la carpeta raíz
+			log.debug("Ruta = " + ruta);
+			
+			dir = new File (ruta);
+			carpeta = new File(dir.getAbsolutePath());
+			lista_programas = carpeta.listFiles();//saco todas las subcarpetas, donde a su vez, estarán los programas
+			
+			str_fecha_mayor = "0"; //INIICALIZo _> Este será el mayor
+			
+			//log.debug("Mayor = " + str_fecha_mayor);
+			for (File fichero : lista_programas)//OJO, precondición importante: mantener en la ruta_programs, sólo carpetas de programas! y nada más
+			{
+				str_aux = fichero.getName();
+				if (str_aux.compareTo(str_fecha_mayor)>0) //si str_aux es mayor, pasa a ser la mayor fecha (el podcast más antiguo ;)
+				{
+					str_fecha_mayor = str_aux;
+				}
+				//log.debug("Mayor = " + str_fecha_mayor);
+			}
+			
+			log.debug("Mayor al final = " + str_fecha_mayor);
+			
+		return str_fecha_mayor;
+	}
+
+	/**
+	 * Devolvemos la fecha del podcast más reciente presente en el servidor en formato plano
+	 * 
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String str_fecha_ultimo = null;
+		PrintWriter pw = null;
+		
+			log.info("Entrando en ObtenerFechaUltimo");
+			
+			response.setContentType(MediaType.TEXT_PLAIN);
+			str_fecha_ultimo = obtenerFechaUltimo ();
+			pw = response.getWriter();
+			pw.write(str_fecha_ultimo);
+			
+			log.info("Mayor devuelto = " + str_fecha_ultimo);
+		
+	}
+
+
+}
---
+++ RadioMarcaServer/src/com/val/ebtm/dto/InfoDescarga.java
@@ -0,0 +1,51 @@
+package com.val.ebtm.dto;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * Created by vale on 18/03/16.
+ */
+public class InfoDescarga implements Serializable
+{
+    private static final long serialVersionUID = 579868457300286134L;
+
+    private InfoUsuario info_usuario;
+    private String fecha; //esto viene seteado en el cliente, id la fecha del podacast solicitado
+    private String momento; //el momento en que se produce la petición
+
+
+    public String getMomento() {
+        return momento;
+    }
+
+    public String getFecha ()
+    {
+        return fecha;
+    }
+
+
+    public void setMomento(Calendar calendar)
+    {
+        DateFormat dateFormat = null;
+
+        dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        momento = dateFormat.format(calendar.getTime());
+    }
+
+
+    public InfoDescarga(InfoUsuario info_usuario, String fecha)
+    {
+        this.info_usuario = info_usuario;
+        this.fecha = fecha;
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return (this.info_usuario.toString() + " Fecha = " +fecha);
+    }
+}
